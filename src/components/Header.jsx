ecafaa6fea5ceab448e4e11e90fbc4e8260faa4f
Trithereon <eliottrassa@gmail.com>
2026-07-29 00:11:14 -0400

    feat: add Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,32 @@
 import styles from "../styles/header.module.css";
+import logo from "../assets/img/logo.png";
 
 function Header() {
-  return <header className={styles.header}>This is the header</header>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerLogo}>
+        <img src={logo} alt="logo" />
+      </div>
+      <div className={styles.headerTitle}>
+        <h1>Ressources de la Vallée</h1>
+        <span>Sainte-Anne-de-la-Rochelle, QC</span>
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.headerActions}>
+        <ul>
+          <li>
+            <button onClick={() => alert("Clicked Home")}>Home</button>
+          </li>
+          <li>
+            <button onClick={() => alert("Clicked Shop")}>Shop</button>
+          </li>
+          <li>
+            <button onClick={() => alert("Clicked Cart")}>Cart</button>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
